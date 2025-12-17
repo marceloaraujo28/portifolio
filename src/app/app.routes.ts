ecddbfb162a711d3563ddbd4f03a4369b13aa2a1
Marcelo Araújo <marcelo_araujo28@hotmail.com>
2025-12-16 22:20:31 -0300

    init portifolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Project } from './pages/project/project';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home
+    },
+    {
+        path: "project/:id",
+        component: Project
+    },
+];
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      colors: {
+        bg: {
+          primary: '#1B1D23',   // fundo principal
+          secondary: '#1F232A', // cartões, blocos
+          alt: '#20242A',       // variação
+          subtle: '#1D2026',    // bordas, profundidade
+        },
+      },
+    },
+  },
+  plugins: [],
+}
