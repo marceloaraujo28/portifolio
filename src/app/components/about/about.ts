9cab7844ecc63535281fa214a443bf6aa3d5f0d7
Marcelo Araújo <marcelo_araujo28@hotmail.com>
2026-03-16 14:56:22 -0300

    feat: added contact and change bout description

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgxMarqueeComponent } from '@omnedia/ngx-marquee';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NgxMarqueeComponent],
+  imports: [CommonModule, NgxMarqueeComponent],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
+  isContactModalOpen = false;
 
+  openContactModal() {
+    this.isContactModalOpen = true;
+  }
+
+  closeContactModal() {
+    this.isContactModalOpen = false;
+  }
 }
