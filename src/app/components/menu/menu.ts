306ce66d88efe08a8a3e490e404956ff5c50788b
Marcelo Araújo <marcelo_araujo28@hotmail.com>
2026-03-16 20:13:43 -0300

    refactor: menu mobile

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  standalone:true,
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
-  
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
